1a7240a29794e0f029926941dbd0a724cf88914d
lakshyabhushan7 <lakshyabhushan379@gmail.com>
2026-07-04 23:17:12 +0530

    Time: 0 ms (100%), Space: 11.1 MB (90.28%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string ans = "";
+        
+        for (char c : num) {
+            while (ans.length() > 0 && ans.back() > c && k > 0) {
+                ans.pop_back();
+                k--;
+            }
+            
+            if (ans.length() > 0 || c != '0') {
+                ans.push_back(c);
+            }
+        }
+        
+        while (ans.length() > 0 && k > 0) {
+            ans.pop_back();
+            k--;
+        }
+        
+        return ans == "" ? "0" : ans;
+    }
+};
