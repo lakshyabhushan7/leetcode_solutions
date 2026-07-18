e2c697053c8128a00c3be3ed85100951544ccd35
lakshyabhushan7 <lakshyabhushan379@gmail.com>
2026-07-18 23:43:44 +0530

    Time: 0 ms (100%), Space: 31.3 MB (44.75%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -12,28 +12,11 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if (!root) return 0;
-        
-        int leftHeight = 0;
-        int rightHeight = 0;
-        
-        TreeNode* leftPtr = root;
-        TreeNode* rightPtr = root;
-        //checking max left hieght
-        while (leftPtr) {
-            leftHeight++;
-            leftPtr = leftPtr->left;
+        if (root == NULL) {
+            return 0;
         }
-        // checking max right hieght
-        while (rightPtr) {
-            rightHeight++;
-            rightPtr = rightPtr->right;
-        }
-        //if tree is perfect left ==right
-        if (leftHeight == rightHeight) {
-            return pow(2,leftHeight) - 1;
-        }
-        // nahi to basic reccursion se no of node
-        return 1 + countNodes(root->left) + countNodes(root->right);
+        int ans= 1 + countNodes(root->left) + countNodes(root->right);
+
+        return ans;
     }
 };
